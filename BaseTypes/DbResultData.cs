42cf308b5dab9022048d6bbb9807c764663b9db1
mertagca <mrtagcaa@gmail.com>
2024-03-09 19:57:06 +0300

    Add project files.

---
+++ BaseTypes/DbResultData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BaseTypes
+{
+    public class DbResultData<T> : DbResultBase
+    {
+        public DbResultData() : base()
+        { }
+        public DbResultData(DbResultBase baseObj, T data = default) : base(baseObj)
+        {
+            Data = data;
+        }
+        public DbResultData(SqlTransaction dbTran) : base(dbTran)
+        { }
+        public DbResultData(Exception exp, SqlTransaction dbTran) : base(exp, dbTran)
+        { }
+        private T _data;
+        public T Data { get => _data; set { _data = value; } }
+    }
+}
